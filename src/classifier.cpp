5e76c4971314b40b04141e1375397ff7a3f30417
LEAFERx <leaferx@outlook.com>
2020-06-04 21:57:07 +0800

    finish

--- src/classifier.cpp
+++ src/classifier.cpp
@@ -36,6 +36,6 @@ opticsData myClassifier::transfer(volumeData v_data, float dt, Camera* cam, std:
 
   float relative_gradient_norm = v_data.gradient.norm() / grad_max_norm;
   optics.color = 5 * (ambient + diffuse + specular) * dt * v_data.density * (1.0f + 0.05f * relative_gradient_norm);
-  optics.transparency = Eigen::Vector3f::Ones() * std::exp(-v_data.density * dt) *(1.0f + 0.05f * relative_gradient_norm);
+  optics.transparency = Eigen::Vector3f::Ones() * std::exp(-v_data.density * dt) * (1.0f + 0.05f * relative_gradient_norm);
   return optics;
 }
